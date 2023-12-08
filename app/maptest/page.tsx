3f80d5899ad3b341fd1cceb5c5a53992e4e024f5
kaylieee <kaylieleung1@gmail.com>
2023-12-08 10:14:39 -0500

    fix file location

---
+++ app/maptest/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import Map from './map.js';
+
+export default function Home(){
+    return(
+        <div>
+            <h1>My Page</h1>
+            <Map address="1600 Amphitheatre Parkway, Mountain View, CA" />
+        </div>
+    )
+}
