fa77cc00db2f623b10202bb11eb2a4268544c6a6
nbockert <123121675+nbockert@users.noreply.github.com>
2023-12-11 18:30:40 -0500

    Added buttons from api response

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import Head from 'next/head'
 
 'use client';
 import Image from 'next/image'
-import { useState, useEffect } from 'react'
+import { useRef, useState, useEffect, FormEvent } from 'react'
+import ReactDOM from 'react-dom'
+
+
 
 let currentLat = 0
 let currentLong = 0
@@ -16,6 +19,7 @@ export default function Home() {
       console.log(currentLat)
     });
   })
+  //ReactDOM.render(map, document.getElementById("mapcontainer"));
   return (
     <div  id='container'>
       <body id="body">
@@ -26,11 +30,13 @@ export default function Home() {
         </div>
         <div id="bottom">
           <div id="leftContainer">
-            <button className={'options'} onClick={() => setCat("library")}>Library</button>    
-            <button className={'options'} onClick={() => setCat("park")}>Park</button> 
-            <button className={'options'} onClick={() => setCat("cafe")}>Cafe</button> 
+            <button className={'options'} onClick={() => setCat('education.library')}>Library</button>    
+            <button className={'options'} onClick={() => setCat("leisure")}>Park</button> 
+            <button className={'options'} onClick={() => setCat('commercial.food_and_drink.coffee_and_tea,commercial.food_and_drink.bakery,catering.cafe.coffee_shop')}>Cafe</button>
+            <button className={'options'} onClick={() => setCat('catering.restaurant')}>Restaurants</button> 
+            <button className={'options'} onClick={() => setCat("commercial")}>Shopping</button>     
             <button className={'options'} onClick={() => setCat("tourism")}>Tourism</button> 
-            <button className={'options'} onClick={() => setCat("entertainment")}>Entertainment</button>
+            <button className={'options'} onClick={() => setCat("entertainment")}>Entertainment</button> 
           </div>
           <div id="centerContainer">
             <div id="mapcontainer"></div>
@@ -53,9 +59,18 @@ const setCat = (cat: string) =>{
   category = cat
   console.log(category)
 }
-
-export async function sendReq(e: Event, a: string){
-  placeDisplay = ''
+type Place = {
+  name: string;
+  address_line2: string;
+  city: string;
+  state: string;
+  country: string;
+  lat: number;
+  lon: number;
+};
+let placeDisplay = ''
+export async function sendReq(e: FormEvent<HTMLFormElement>, a: string){
+  
   e.preventDefault();
   const res = await fetch("http://localhost:3000/api/address", {
     method: "POST",
@@ -70,15 +85,55 @@ export async function sendReq(e: Event, a: string){
     }
   })
   const data = await res.json()
-  const placesArray = data.responseData.properties
-  handleData(placesArray)
-}
+  const placesArray = data.responseData.properties;
+  const placeElements = placesArray.map((place: Place, index: number) => (
+    <div key={index} className="place">
+      <button className="bold-button" onClick={() => marker(place.lat,place.lon)}><span className="text">{place.name}</span></button>
+      <p>{place.address_line2}</p>
+    </div>
+  ));
+  // for (let i = 0; i < placesArray.length; i++) {
+  //   const place = getAddressHTML(placesArray[i]);
+  //   placeDisplay += ReactDOMServer.renderToStaticMarkup(place);
+  // }
+  ReactDOM.render(placeElements, document.getElementById('dest'));
 
-let placeDisplay = ''
-function handleData(places: Array<JSON>){
-  for(let i = 0; i < places.length; i++){
-    placeDisplay += '<div class=place><h1>'+places[i].name+'</h1>'+'<p>'+places[i].address_line2.split(',')[0]+'</p></div>'
-  }
-  console.log(placeDisplay)
-  document.getElementById('dest').innerHTML = placeDisplay
+  
+}
+// export function Map() {
+//   const mapContainer = useRef(null);
+//   const map = useRef(null);
+//   const [lng] = useState(currentLong);
+//   const [lat] = useState(currentLat);
+//   const [zoom] = useState(14);
+//   const [API_KEY] = useState();
+
+//   useEffect(() => {
+//     if (map.current) return; // stops map from intializing more than once
+
+
+//     map.current = new maplibregl.Map({
+//       container: mapContainer.current,
+//       style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${API_KEY}`,
+//       center: [lng, lat],
+//       zoom: zoom
+//     });
+
+//   }, [API_KEY, lng, lat, zoom]);
+
+//   return (
+//     <div className="map-wrap">
+//       <div ref={mapContainer} className="map" />
+//     </div>
+//   );
+// }
+
+
+function marker(lat: number, long: number){
+  //placeDisplay = '<div class=place><h1>'+ places.name+'</h1>'+'<p>'+places.address_line2.split(',')[0]+'</p></div>'
+  //document.getElementById("dest")?.innerHTML(placeDisplay)
+  //console.log(placeDisplay)
+  console.log("hey")
+
+
 }
