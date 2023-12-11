629dd0bcdcf1dd2339df477d3cc3f6e4f8b97387
kaylieee <kaylieleung1@gmail.com>
2023-12-10 21:04:12 -0500

    added category handling, added api response handling, fixed formatting

--- app/page.tsx
+++ app/page.tsx
@@ -22,11 +22,11 @@ export default function Home() {
         <button className={'options'} onClick={() => setCat("tourism")}>Tourism</button> 
         <button className={'options'} onClick={() => setCat("entertainment")}>Entertainment</button> 
         <button className={'gen'}>Generate Places!</button>
-        <form onSubmit={() => sendReq(address)} className={'form'}>
-          <input type="text" name="address" onChange = {e => {setAddress(e.currentTarget.value);}} className={'input'}></input>
+        <form onSubmit={(e) => sendReq(e, address)} className={'form'}>
+          <input type="text" name="address" onChange = {(e) => {setAddress(e.currentTarget.value);}} className={'input'}></input>
           <button type="submit" name="submit">submit</button>
         </form>
-        <div id='addressContainer'></div>
+        <div className={'destinations'} id='dest'>{placeDisplay}</div>
       </body>
     </div>
   )
@@ -38,8 +38,9 @@ const setCat = (cat: string) =>{
   console.log(category)
 }
 
-function sendReq(a: string){
-  fetch("http://localhost:3000/api/hello", {
+export async function sendReq(e: Event, a: string){
+  e.preventDefault();
+  const res = await fetch("http://localhost:3000/api/address", {
     method: "POST",
     body: JSON.stringify({
       lat: currentLat,
@@ -51,6 +52,16 @@ function sendReq(a: string){
       "Content-type": "application/json; charset=UTF-8"
     }
   })
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+  const data = await res.json()
+  const placesArray = data.responseData.properties
+  handleData(placesArray)
+}
+
+let placeDisplay = ''
+function handleData(places: Array<JSON>){
+  for(let i = 0; i < places.length; i++){
+    placeDisplay += '<h1>'+places[i].name+'</h1>'+'<p>'+places[i].address_line2+'</p>'
+  }
+  console.log(placeDisplay)
+  document.getElementById('dest').innerHTML = placeDisplay
 }
--- pages/api/address.js
+++ pages/api/address.js
@@ -20,92 +20,105 @@ export default async function handler(req, res) {
     //geocode address entered in form 
     const radarApiKey = ''; // Replace with your Radar API key
     const radarApiUrl = `https://api.radar.io/v1/geocode/forward?query=${encodeURIComponent(address)}`;
+    
     //api call to radar
     const radarApiResponse = await fetch(radarApiUrl, {
       method: 'GET',
       headers: {
           'Authorization': radarApiKey,
         },
-        });
-  
-        const radarApiData = await radarApiResponse.json();
-  
-        // Extract coordinates from the Radar API response
-        const { latitude, longitude } = radarApiData.addresses[0];
-  
-        console.log('Coordinates:', { latitude, longitude });
-        //formatting the response to return a json with a list of places 
-        const responseData = {
-          //calling getPlaces to generate places
-          properties: getPlaces(lat,long,latitude,longitude,category),
-        }
-        
-  
+      });
+
+      const radarApiData = await radarApiResponse.json();
+      // Extract coordinates from the Radar API response
+      const { latitude, longitude } = radarApiData.addresses[0];
+      console.log('Coordinates:', { latitude, longitude });
+
+      const cat = parseCategory(category);
+      //formatting the response to return a json with a list of places 
+      const responseData = {
+        //calling getPlaces to generate places
+        properties: getPlaces(lat,long,latitude,longitude,cat),
+      }
+
         // Respond with the coordinates or any other data you need
-        res.status(200).json({ responseData });
+      console.log(responseData)
+      res.status(200).json({ responseData });
       } catch (error) {
         console.error('Error calling Radar API:', error);
         res.status(500).json({ message: 'Internal Server Error' });
-    
     }
-    
   }
 
-  function getPlaces(lat,long,lat1,long1,cat){
-    //const nu = prompt("How many places do you want to search for?:");
-    //let num = parseInt(nu);
-    fetch('https://api.geoapify.com/v2/places?categories='+cat+'&filter=rect:'+long+','+lat+','+long1+','+lat1+'&bias=proximity:'+long+','+lat+'&limit=3&apiKey=')
-    .then(resp => resp.json())
-      .then((places) => {
-        //iterate through all of the places returned by geoapify
-        places.features.map((place) => {
-          console.log(place)
-          if (place.properties !== undefined) {
-            //if the place is valid create variables for all of the properties for each place 
-            const {
-              name,
-              address_line2,
-              city,
-              state,
-              country,
-              lat,
-              lon,
-            } = place.properties;
-            //if the place hasn't been listed before and is not undefinded set the properties of the place into json format 
-            if(arraybool(name)){
-              const newPlace = {
-                name: name,
-                address_line2: address_line2,
-                city: city,
-                state: state,
-                country: country,
-                lat: lat,
-                lon: lon,
-              };
-              //add place to array of places
-              array.push(newPlace);
-            }
+function getPlaces(lat,long,lat1,long1,cat){
+  //const nu = prompt("How many places do you want to search for?:");
+  //let num = parseInt(nu);
+  fetch('https://api.geoapify.com/v2/places?categories='+cat+'&filter=rect:'+long+','+lat+','+long1+','+lat1+'&bias=proximity:'+long+','+lat+'&limit=3&apiKey=')
+  .then(resp => resp.json())
+    .then((places) => {
+      //iterate through all of the places returned by geoapify
+      places.features.map((place) => {
+        if (place.properties !== undefined) {
+          //if the place is valid create variables for all of the properties for each place 
+          const {
+            name,
+            address_line2,
+            city,
+            state,
+            country,
+            lat,
+            lon,
+          } = place.properties;
+          //if the place hasn't been listed before and is not undefinded set the properties of the place into json format 
+          if(arraybool(name)){
+            const newPlace = {
+              name: name,
+              address_line2: address_line2,
+              city: city,
+              state: state,
+              country: country,
+              lat: lat,
+              lon: lon,
+            };
+            //add place to array of places
+            array.push(newPlace);
           }
-          
-        });
+        }
         
       });
-      //return it and send it as a response 
-      return array;
-  }
+      
+    });
+    //return it and send it as a response 
+    return array;
+}
 
 
-  
-  function arraybool(name){
-    if(name==="undefined"){
-      return false;
-    }
-    if(!track.includes(name)){
-      track.push(name);
-      return true;
-    }
-    else{
-      return false;
-    }
+
+function arraybool(name){
+  if(name==="undefined"){
+    return false;
   }
-  
+  if(!track.includes(name)){
+    track.push(name);
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+function parseCategory(c){
+  if(c == "library"){
+    return 'education.library';
+  }
+  if(c == "entertainment"){
+    return 'entertainment';
+  }
+  if(c == "park"){
+    return 'leisure.park'
+  }
+  if(c=="cafe"){
+    return 'commercial.food_and_drink';
+  }
+}
+
