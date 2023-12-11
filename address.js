7029a7aa8ee519b59c4f44b6ab34a0e3df23ceeb
nbockert <123121675+nbockert@users.noreply.github.com>
2023-12-11 18:49:01 -0500

    Ignore this file

--- address.js
+++ address.js
@@ -1,38 +1,33 @@
 
 import React from 'react';
 import Radar from 'radar-sdk-js';
+//const axiosRequest = require("axios");
+
+
+{
+  var array = new Array();
+}
+{
+  var track = new Array();
+}
+
 
 
 // pages/api/address.js
 
 export default async function handler(req, res) {
-//     if (req.method === 'GET') {
-//         try {
-//           const { cat, home_lat, home_long } = req.query;
-//           console.log(cat,home_lat,home_long);
-//           res.status(200).json({ cat, home_lat, home_long });
-
-//     }
-//         catch (error) {
-//         console.error('Error calling Radar API:', error);
-//         res.status(500).json({ message: 'Internal Server Error' });
-//     }
-// }
-    
-    if (req.method === 'POST') {
-      try {
-        console.log(req.data);
-        const { address } = req.body;
-  
-        // Call the Radar API for forward geocoding
-        const radarApiKey = 'prj_live_pk_983a93248e6cd3385f674c44fd9e885e0cc8b191'; // Replace with your Radar API key
-        const radarApiUrl = `https://api.radar.io/v1/geocode/forward?query=${encodeURIComponent(address)}`;
-  
-        const radarApiResponse = await fetch(radarApiUrl, {
-          method: 'GET',
-          headers: {
-            'Authorization': radarApiKey,
-          },
+  try {
+    console.log(req.body);
+    const { lat, long, address, category } = req.body;
+    //geocode address entered in form 
+    const radarApiKey = ''; // Replace with your Radar API key
+    const radarApiUrl = `https://api.radar.io/v1/geocode/forward?query=${encodeURIComponent(address)}`;
+    //api call to radar
+    const radarApiResponse = await fetch(radarApiUrl, {
+      method: 'GET',
+      headers: {
+          'Authorization': radarApiKey,
+        },
         });
   
         const radarApiData = await radarApiResponse.json();
@@ -40,19 +35,62 @@ export default async function handler(req, res) {
         // Extract coordinates from the Radar API response
         const { latitude, longitude } = radarApiData.addresses[0];
   
-        // You can use latitude and longitude for further processing
         console.log('Coordinates:', { latitude, longitude });
+        //formatting the response to return a json with a list of places 
+        let newarr = getPlaces(lat,long,latitude,longitude,category)
+        res.status(200).json({ text: "hello" })
+        //res.status(200).send(newarr);
+       
+        
   
         // Respond with the coordinates or any other data you need
-        res.status(200).json({ latitude, longitude });
+        
       } catch (error) {
         console.error('Error calling Radar API:', error);
         res.status(500).json({ message: 'Internal Server Error' });
-      }
-    }
-    else {
-        res.status(405).json({ message: 'Method Not Allowed' });
-      } 
     
+    }
+    
+  }
+
+  async function getPlaces(lat,long,lat1,long1,cat){
+    //const nu = prompt("How many places do you want to search for?:");
+    //let num = parseInt(nu);
+    try{
+      let response = await fetch('https://api.geoapify.com/v2/places?categories='+cat+'&filter=rect:'+long+','+lat+','+long1+','+lat1+'&bias=proximity:'+long+','+lat+'&limit=3&apiKey=')
+      const places = await response.json();
+      places.features.forEach((place) => {
+        if (place.properties !== undefined) {
+          const { name, address_line2, city, state, country, lat, lon } = place.properties;
+          if (arraybool(name)) {
+            array.push(place.properties);
+          }
+        }
+      });
+      console.log(array);
+      return array;
+    }
+      catch (error) {
+        console.error('Error fetching data:', error);
+        // Handle error appropriately
+        return [];
+      }
+
+    }
+    
+
+
+  
+  function arraybool(name){
+    if(name==="undefined"){
+      return false;
+    }
+    if(!track.includes(name)){
+      track.push(name);
+      return true;
+    }
+    else{
+      return false;
+    }
   }
   
